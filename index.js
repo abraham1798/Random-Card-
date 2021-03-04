78ec2e7aef174b7f3e9911d2ed11724a9d59308b
abraham1798 <abrahamc.a@hotmail.com>
2021-03-04 21:32:26 +0000

    Trabajo Finalizado

---
+++ index.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    let numbers = ['A',1,2,3,4,5,6,7,8,9,10,'J','Q','K']; //14
+    let number = document.querySelector('#number');
+    let random = getRandom(11);
+    let symbol = getSymbol(getRandom(4));
+    let top = document.querySelector('.top');
+    let bottom = document.querySelector('.bottom');
+    if(symbol==='\u2660' || symbol==='\u2663'){
+        top.style.color = 'black';
+        bottom.style.color = 'black';
+    }
+    number.innerHTML = numbers[getRandom(14)];
+    top.innerHTML = symbol;
+    bottom.innerHTML = symbol;
+    
+};
+function getRandom(max){
+    return Math.floor(Math.random() * (max - 1) + 1);
+}
+function getSymbol(option){
+    switch (option) {
+        case 1:
+            let spades ='\u2660';
+            return spades;
+            break;
+        case 2:
+            let trebols ='\u2663';
+            return trebols;
+            break;
+        case 3:
+            let hearts ='\u2665';
+            return hearts;
+            break;
+    
+        default:
+            hearts ='\u2665';
+            return hearts;
+            break;
+    }
+}
